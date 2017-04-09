6fee638eaf0eea9973353546d004bb756628ff70
Nick-Watson <nickwatson248@gmail.com>
2017-04-09 19:46:46 +0100

    added cors middleware

--- server.js
+++ server.js
@@ -19,10 +19,11 @@ mongoose.connect(db, function (err) {
 
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    next();
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
 app.use('/api', apiRouter);
